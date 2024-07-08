220ddaf7fe0cb378c3da51eb88813a37f1254691
crob127 <164558528+crob127@users.noreply.github.com>
2024-07-07 23:06:01 -0400

    fixed column definitions

--- models/Category.js
+++ models/Category.js
@@ -5,7 +5,6 @@ const sequelize = require('../config/connection.js');
 class Category extends Model {}
 
 // define columns
-
 Category.init(
   {
     id: {
@@ -28,107 +27,4 @@ Category.init(
   }
 );
 
-class Product extends Model {}
-
-Product.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    product_name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    price: {
-      type: DataTypes.Decimal,
-      allowNull: false,
-      validate: {
-        isDecimal: true,
-      },
-    },
-    stock: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      defaultValue: 10,
-      validate: {
-        isNumeric: true,
-      },
-    },
-    category_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: 'category',
-        key: 'id',
-      },
-    },
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'product',
-  }
-);
-
-class Tag extends Model {}
-
-EventTarget.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    tag_name: {
-      type: DataTypes.STRING,
-    },
-  },
-  {
-    sequalize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'tag',
-  }
-);
-
-class ProductTag extends Model {}
-
-ProductTag.init (
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    product_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: 'product',
-        key: 'id',
-      },
-    },
-    tag_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: 'tag',
-        key: 'id',
-      },
-    },
-  },
-  {
-    sequalize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'product_tag',
-  }
-);
-
-module.exports = { Category, Product, Tag, ProductTag };
+module.exports = Category;
